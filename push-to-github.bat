a6fe83e33e8af21c1967264090c06e7cb428f641
takumiksk009 <yanguadaruta@gmail.com>
2025-07-10 18:38:01 +0900

    在庫登録の修正

--- push-to-github.bat
+++ push-to-github.bat
@@ -1,7 +1,7 @@
 @echo off
-cd /d "C:\Users\KSK\Desktop\LINEWO~1\在庫管~2"
+cd /d "C:\Users\KSK\Desktop\LINE WORKSアプリ\在庫管理 簡易版"
 echo === 変更をGitに追加中... ===
-git add .
+git add -A
 
 echo.
 set /p commitmsg=コミットメッセージを入力してください（例：在庫ページ更新）: 
@@ -9,7 +9,7 @@ git commit -m "%commitmsg%"
 
 echo.
 echo === GitHubにPush中... ===
-git push
+git push origin main
 
 echo.
 echo === 完了しました！お疲れさまです♪ === 
